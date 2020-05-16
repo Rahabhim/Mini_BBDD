4bb1782caf00f67d838fc9eee6c9483832c78d65
Max <maxiolaz@gmail.com>
2020-05-16 15:32:53 -0300

    Creacion de modulo temas.py

---
+++ temas.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+def EleccionTema(var_temas):
+    if var_temas.get() == 1:
+        return "red"
+
+    elif var_temas.get() == 2:
+        return "blue"
+
+    elif var_temas.get() == 3:
+        return "green"
