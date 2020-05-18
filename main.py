25a8219195ba4383f9f4b25609fe3bc2583b46d0
Max <maxiolaz@gmail.com>
2020-05-18 03:04:10 -0300

    Agregada info de alumno

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+
+#-----------------------------------------------------------------------------#
+#------------- Alumno: Maximiliano Gabriel Olaz Bondarczuk -------------------#
+#-----------------------------------------------------------------------------#
+#------------------- Tarea unidad 6 | Nivel Intermedio -----------------------#
+#-----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+#------------------------------ Modificaciones -------------------------------#
+# -Patron MVC                                                                 #
+# -Implementacion de shelve en el modulo temas                                #
+# -Agregada la opcion para modificar y borrar                                 #
+# -Agregada la opcion para generar un PDF con los datos guardados             #
+#-----------------------------------------------------------------------------#
+
+
 from tkinter import *
 from tkinter import ttk
 from MVC.controlador import controlador
